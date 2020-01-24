0a660969c910ef4e46c301de730511b5ec920fb7
tophfinch <tophfinch@gmail.com>
2020-01-24 12:48:14 -0700

    card animation

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -17,12 +17,17 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+import { useSpring, animated } from 'react-spring';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
 
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
 function RenderCampsite({ campsite }) {
+  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
   return (
     <div className="col-md-5 m-1">
       <FadeTransform
@@ -30,12 +35,19 @@ function RenderCampsite({ campsite }) {
           transformProps={{
               exitTransform: 'scale(0.5) translateY(-50%)'
           }}>
-          <Card>
-              <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-              <CardBody>
-                  <CardText>{campsite.description}</CardText>
-              </CardBody>
-          </Card>
+          <animated.div
+      class="card"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: props.xys.interpolate(trans) }}
+         >
+            <Card>
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+          </animated.div>
       </FadeTransform>
     </div>
   );
